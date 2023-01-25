0d9268d9fdb327ebfad946f3c50f746fe061027f
Mofolabi Osotimehin <mofolabi.osotimehin@student.moringaschool.com>
2023-01-25 14:34:14 +0100

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+// find matching
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase().includes(name.toLowerCase()));
+}
+
+
+// fuzzy match
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+// matchName
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
